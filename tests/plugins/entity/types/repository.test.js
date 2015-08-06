36c5cf8d53125333970dca171d5535129121b889
Jacob Mather <jmather@jmather.com>
2015-08-06 16:09:48 -0700

    Test coverage

--- tests/plugins/entity/types/repository.test.js
+++ tests/plugins/entity/types/repository.test.js
@@ -1,98 +1,71 @@
-describe('CollectionType', function() {
-    return;
+describe('RepositoryType', function() {
     var DepBuilder = require.main.require('test-data/bootstrap/builder');
-    var plugins = DepBuilder.loadPlugins(['collection', 'counter']);
+    var plugins = DepBuilder.loadPlugins(['entity', 'counter']);
     var data;
 
     beforeEach(function() {
         data = DepBuilder.byFileName(__filename, plugins);
     });
 
-    it('Instantiates correctly', function() {
-        var collection = data.valueManager.get('smallGrid');
-    });
-
-    describe('getAllCellIds', function() {
-        it('reports 9 cells, 1-1 through 3-3 for smallGrid', function() {
-            var expected = ['1-1', '1-2', '1-3', '2-1', '2-2', '2-3', '3-1', '3-2', '3-3'];
-            var grid = data.valueManager.get('smallGrid');
-            var cells = grid.getAllCellIds();
-            cells.should.eql(expected);
-            cells.length.should.equal(9);
-            cells[0].should.equal('1-1');
-            cells[8].should.equal('3-3');
+    describe('constructor', function() {
+        it('Instantiates correctly', function() {
+            var collection = data.valueManager.get('entity.thing');
         });
 
-        it('reports 100 cells, 1-1 through 10-10 for largeGrid', function() {
-            var grid = data.valueManager.get('largeGrid');
-            grid.getAllCellIds().length.should.equal(100);
+        it('sets the initial size to zero', function() {
+            data.valueManager.get('entity.thing.size').getValue().should.equal(0);
+        });
+
+        it('sets the last id to zero', function() {
+            data.valueManager.get('entity.thing.lastId').getValue().should.equal(0);
         });
     });
 
-    describe('getAvailableCellIds', function() {
-        it('reports 9 cells initially for smallGrid', function() {
-            var grid = data.valueManager.get('smallGrid');
-            grid.getAvailableCellIds().length.should.equal(9);
-        });
-
-        it('reports 8 once we have set a cell', function() {
-            var grid = data.valueManager.get('smallGrid');
-            grid.setCell(1, 1, 'hello');
-            grid.getAvailableCellIds().length.should.equal(8);
-        })
-    });
-
-    describe('getOccupiedCellIds', function() {
-        it('reports 0 cells initially for smallGrid', function() {
-            var grid = data.valueManager.get('smallGrid');
-            grid.getOccupiedCellIds().length.should.equal(0);
-        });
-
-        it('reports 1 cell once set', function() {
-            var grid = data.valueManager.get('smallGrid');
-
-            grid.setCell(1, 1, 'hello');
-
-            grid.getOccupiedCellIds().length.should.equal(1);
-        });
-    });
-
-    describe('getCell', function() {
-        var grid;
+    describe('insert', function() {
+        var repository, thing, ref;
 
         beforeEach(function() {
-            grid = data.valueManager.get('smallGrid');
+            repository = data.valueManager.get('entity.thing');
+            thing = {a: 'b'};
+            ref = repository.insert(thing);
         });
 
-        it('returns null when not set', function() {
-            (grid.getCell(1, 1) === null).should.equal(true);
+        it('returns a reference on insertion', function() {
+            var expected = { value: 'entity.thing', id: 1 };
+            ref.should.eql(expected);
         });
 
-        it('returns the assigned value when set', function() {
-            var data = {test: "payload"};
-            grid.setCell(1, 1, data);
+        it('increments the size by one', function() {
+            data.valueManager.get('entity.thing.size').getValue().should.equal(1);
+        });
 
-            grid.getCell(1, 1).should.eql(data);
+        it('sets the lastId to 1', function() {
+            data.valueManager.get('entity.thing.lastId').getValue().should.equal(1);
         });
     });
 
-    describe('setCell', function() {
-        var grid;
+    describe('removeByReference', function() {
+        var repository, thing, ref;
 
         beforeEach(function() {
-            grid = data.valueManager.get('smallGrid');
+            repository = data.valueManager.get('entity.thing');
+            thing = {a: 'b'};
+            ref = repository.insert(thing);
+            repository.removeByReference(ref);
         });
 
-        it('it returns null on first assignment', function() {
-            var data = {test: "payload"};
-            (grid.setCell(1, 1, data) === null).should.equal(true);
+        it('removes the entity from the repo', function() {
+            var repoData = repository.getValue();
+
+            (typeof repoData[ref.id]).should.equal("undefined");
         });
 
-        it('it returns the prior value on next assignment', function() {
-            var data = {test: "payload"};
-
-            grid.setCell(1, 1, data);
-            grid.setCell(1, 1, true).should.eql(data);
+        it('reduces size back to zero', function() {
+            data.valueManager.get('entity.thing.size').getValue().should.equal(0);
         });
-    })
+
+        it('does not change lastId', function() {
+            data.valueManager.get('entity.thing.lastId').getValue().should.equal(1);
+        });
+    });
 });
