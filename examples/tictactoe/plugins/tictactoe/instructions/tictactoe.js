8ddf61cc2ff3e88ee0be69ac50d889e488cb7493
Jacob Mather <jmather@jmather.com>
2015-08-28 20:44:45 -0700

    Fix tic-tac-toe validator and make a nice way to update the config on the VM

--- examples/tictactoe/plugins/tictactoe/instructions/tictactoe.js
+++ examples/tictactoe/plugins/tictactoe/instructions/tictactoe.js
@@ -47,22 +47,38 @@ function checkDiagonals(grid, min) {
     var values = grid.getOccupiedCellIds();
 
     var lr_diagonal = 0;
+    var lastLR = null;
+    var maxLR = 0;
     var rl_diagonal = 0;
+    var maxRL = null;
+    var lastRL = 0;
 
     for (var row = 1; row <= grid.config.rows; row++) {
         var lr_diagonal_col = row;
         var rl_diagonal_col = (grid.config.cols - (row - 1));
 
         if (values.indexOf(row + '-' + lr_diagonal_col) !== -1) {
-            lr_diagonal++;
+            if (lastLR === null || lastLR === row - 1) {
+                lr_diagonal++;
+                maxLR = Math.max(maxLR, lr_diagonal);
+            } else {
+                lr_diagonal = 0;
+            }
+
+            lastLR = row;
         }
 
         if (values.indexOf(row + '-' + rl_diagonal_col) !== -1) {
-            rl_diagonal++;
+            if (lastRL === null || lastRL === row - 1) {
+                rl_diagonal++;
+                maxRL = Math.max(maxRL, rl_diagonal);
+            } else {
+                rl_diagonal = 0;
+            }
         }
     }
 
-    return (lr_diagonal >= min || rl_diagonal >= min);
+    return (maxRL >= min || maxLR >= min);
 }
 
 /**
@@ -72,20 +88,50 @@ function checkDiagonals(grid, min) {
  */
 function checkRowsAndCols(grid, min) {
     var rows = {}, cols = {};
-    _.each(_.range(grid.config.rows), function(i) { rows[i + 1] = 0; });
-    _.each(_.range(grid.config.cols), function(i) { cols[i + 1] = 0; });
+    _.each(_.range(grid.config.rows), function(i) { rows[i + 1] = []; });
+    _.each(_.range(grid.config.cols), function(i) { cols[i + 1] = []; });
+    var lastRow = null;
+    var lastCol = null;
 
     var hasWon = false;
 
     _.each(grid.getOccupiedCellIds(), function(value) {
-        var data = value.split('-');
-        rows[data[0]]++;
-        cols[data[1]]++;
+        if (hasWon === true) {
+            return;
+        }
 
-        if (rows[data[0]] === 3 || cols[data[1]] === 3) {
-            hasWon = true;
+        var data = value.split('-');
+        rows[data[0]].push(data[1]);
+        cols[data[1]].push(data[0]);
+
+        var checkContiguousCells = function(memo, value ) {
+            value = parseInt(value, 10);
+            memo = memo || { nextValue: null, running: 0 };
+            if (memo.nextValue === null || memo.nextValue === value) {
+                memo.nextValue = value + 1;
+                memo.running++;
+                return memo;
+            }
+
+            memo.nextValue = null;
+            memo.running = 0;
+            return memo;
+        };
+
+        if (rows[data[0]].length >= min) {
+            if (_.reduce(rows[data[0]], checkContiguousCells, null).running >= min) {
+                hasWon = true;
+            }
+        }
+
+        if (cols[data[1]].length >= min) {
+            if (_.reduce(cols[data[1]], checkContiguousCells, null).running >= min) {
+                hasWon = true;
+            }
         }
     });
+
+    console.log(rows, cols);
 
     return hasWon;
 }
